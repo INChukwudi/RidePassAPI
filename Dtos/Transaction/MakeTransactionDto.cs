1189fe359df5607257a12de2b4b68840a9320f3a
Chukwudi Ike-Nwako <ikenwakochukwudi@gmail.com>
2023-11-13 16:48:58 +0100

    added the make transaction dto for handling data required for making a transaction

---
+++ Dtos/Transaction/MakeTransactionDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RidePassAPI.Dtos.Transaction
+{
+    public class MakeTransactionDto
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public double Amount { get; set; }
+
+        [Required]
+        public string Sender { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+    }
+}
