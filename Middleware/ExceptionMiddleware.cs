ad49f2b9cd5ee02a2f05979d762166f42a195412
Chukwudi Ike-Nwako <ikenwakochukwudi@gmail.com>
2023-11-13 16:45:41 +0100

    added an exception middleware for proper error handling

---
+++ Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,40 @@
+﻿using RidePassAPI.Responses;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace RidePassAPI.Middleware
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionMiddleware> _logger;
+        private readonly IHostEnvironment _env;
+
+        public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger, IHostEnvironment env)
+        {
+            _next = next;
+            _logger = logger;
+            _env = env;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, e.Message);
+                context.Response.ContentType = "application/json";
+                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+                var response = _env.IsDevelopment() ?
+                    new ApiExceptionResponse((int)HttpStatusCode.InternalServerError, e.Message, e.StackTrace) :
+                    new ApiExceptionResponse((int)HttpStatusCode.InternalServerError, e.Message);
+
+                await context.Response.WriteAsync(JsonConvert.SerializeObject(response));
+            }
+        }
+    }
+}
