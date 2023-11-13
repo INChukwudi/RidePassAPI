5eb57b6295b5a194b27573143788e00afea52fbc
Chukwudi Ike-Nwako <ikenwakochukwudi@gmail.com>
2023-11-13 16:44:46 +0100

    added an errors controller for error handling

---
+++ Controllers/ErrorsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RidePassAPI.Responses;
+
+namespace RidePassAPI.Controllers
+{
+    [Route("errors/{code}")]
+    [ApiExplorerSettings(IgnoreApi = true)]
+    [ApiController]
+    public class ErrorsController : ControllerBase
+    {
+        public IActionResult Error(int code)
+        {
+            return new ObjectResult(new ErrorResponse(code, "An error occured in processing your request!"));
+        }
+    }
+}
