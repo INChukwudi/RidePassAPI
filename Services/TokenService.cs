9a0a1d3c20c5335e558fe0775b38cf9105dcf553
Chukwudi Ike-Nwako <ikenwakochukwudi@gmail.com>
2023-11-13 16:47:02 +0100

    added the implemntation of the token service

---
+++ Services/TokenService.cs
@@ -0,0 +1,48 @@
+﻿using RidePassAPI.Contracts.ServiceContracts;
+using RidePassAPI.Models.IdentityModels;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace RidePassAPI.Services
+{
+    public class TokenService : ITokenService
+    {
+        private readonly IConfiguration _configuration;
+
+        private readonly SymmetricSecurityKey _key;
+
+        public TokenService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Token:Key"] ?? throw new InvalidOperationException("Token key not found.")));
+        }
+
+        public string CreateToken(AppUser user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Email, user.Email),
+                new Claim(ClaimTypes.Role, user.UserType.ToString()),
+                new Claim(ClaimTypes.PrimarySid, user.UserId.ToString()),
+            };
+
+            var cred = new SigningCredentials(_key, SecurityAlgorithms.HmacSha512Signature);
+            var tokenDecriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.UtcNow.AddHours(2),
+                NotBefore = DateTime.UtcNow,
+                SigningCredentials = cred,
+                Issuer = _configuration["Token:Issuer"],
+                Audience = _configuration["Token:Audience"],
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDecriptor);
+
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
