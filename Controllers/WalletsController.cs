dddaf06864f3468984d27cd0daf2d7b3ff9664ba
Chukwudi Ike-Nwako <ikenwakochukwudi@gmail.com>
2023-11-13 16:49:42 +0100

    added the wallets controler

---
+++ Controllers/WalletsController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RidePassAPI.Data;
+using RidePassAPI.Models;
+
+namespace RidePassAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WalletsController : ControllerBase
+    {
+        private readonly RidePassAPIContext _context;
+
+        public WalletsController(RidePassAPIContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Wallets
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Wallet>>> GetWallet()
+        {
+          if (_context.Wallet == null)
+          {
+              return NotFound();
+          }
+            return await _context.Wallet.ToListAsync();
+        }
+
+        // GET: api/Wallets/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Wallet>> GetWallet(int id)
+        {
+          if (_context.Wallet == null)
+          {
+              return NotFound();
+          }
+            var wallet = await _context.Wallet.FindAsync(id);
+
+            if (wallet == null)
+            {
+                return NotFound();
+            }
+
+            return wallet;
+        }
+
+        // PUT: api/Wallets/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutWallet(int id, Wallet wallet)
+        {
+            if (id != wallet.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(wallet).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!WalletExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Wallets
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Wallet>> PostWallet(Wallet wallet)
+        {
+          if (_context.Wallet == null)
+          {
+              return Problem("Entity set 'RidePassAPIContext.Wallet'  is null.");
+          }
+            _context.Wallet.Add(wallet);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetWallet", new { id = wallet.Id }, wallet);
+        }
+
+        // DELETE: api/Wallets/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteWallet(int id)
+        {
+            if (_context.Wallet == null)
+            {
+                return NotFound();
+            }
+            var wallet = await _context.Wallet.FindAsync(id);
+            if (wallet == null)
+            {
+                return NotFound();
+            }
+
+            _context.Wallet.Remove(wallet);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool WalletExists(int id)
+        {
+            return (_context.Wallet?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
