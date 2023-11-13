3ea0234efc16c5111cdb41c621dc1e402a01cb8d
Chukwudi Ike-Nwako <ikenwakochukwudi@gmail.com>
2023-11-13 16:43:02 +0100

    added a model for holding the identity user

---
+++ Models/IdentityModels/AppUser.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace RidePassAPI.Models.IdentityModels
+{
+    public class AppUser : IdentityUser
+    {
+        [Required]
+        public int? UserId { get; set; }
+
+        [Required]
+        public int? UserType { get; set; }
+    }
+
+    public enum UserTypes
+    {
+        Customer,
+        Beneficiary,
+        MinistryAdmin,
+    }
+}
