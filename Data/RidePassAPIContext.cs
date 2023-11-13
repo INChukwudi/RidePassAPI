645de079cc1807607265704a9ad21f6b94db2578
Chukwudi Ike-Nwako <ikenwakochukwudi@gmail.com>
2023-11-13 16:45:21 +0100

    added a data context for the app models and the identity models

---
+++ Data/RidePassAPIContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RidePassAPI.Models;
+
+namespace RidePassAPI.Data
+{
+    public class RidePassAPIContext : DbContext
+    {
+        public RidePassAPIContext (DbContextOptions<RidePassAPIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RidePassAPI.Models.Driver> Driver { get; set; } = default!;
+
+        public DbSet<RidePassAPI.Models.Passenger> Passenger { get; set; } = default!;
+
+        public DbSet<RidePassAPI.Models.Transaction> Transaction { get; set; } = default!;
+
+        public DbSet<RidePassAPI.Models.Wallet> Wallet { get; set; } = default!;
+    }
+}
---
+++ Data/RidePassIdentityContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using RidePassAPI.Models.IdentityModels;
+
+namespace RidePassAPI.Data
+{
+    public class RidePassIdentityContext : IdentityDbContext<AppUser>
+    {
+        public RidePassIdentityContext(DbContextOptions<RidePassIdentityContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
