d6fa920f83d69a4340a92aac307077941613cc1c
Chukwudi Ike-Nwako <ikenwakochukwudi@gmail.com>
2023-11-13 16:48:03 +0100

    added a dto for passing the passenger registration data

---
+++ Dtos/Passenger/RegisterPassengerDto.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RidePassAPI.Dtos.Passenger
+{
+    public class RegisterPassengerDto
+    {
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
