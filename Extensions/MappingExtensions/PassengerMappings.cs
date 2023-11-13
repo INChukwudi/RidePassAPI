24634dbb3ec95ce93d53dab70110e29f3ac8d03f
Chukwudi Ike-Nwako <ikenwakochukwudi@gmail.com>
2023-11-13 16:49:28 +0100

    added an extension method on the passenger class to handle mapping from the register passenger dto

---
+++ Extensions/MappingExtensions/PassengerMappings.cs
@@ -0,0 +1,22 @@
+﻿using RidePassAPI.Dtos.Passenger;
+using RidePassAPI.Models;
+
+namespace RidePassAPI.Extensions.MappingExtensions
+{
+    public static class PassengerMappings
+    {
+        public static Passenger PassengerFromRegisterPassengerDto(this Passenger passenger, RegisterPassengerDto passengerDto)
+        {
+            return new Passenger
+            {
+                Email = passengerDto.Email,
+                PhoneNumber = passengerDto.PhoneNumber,
+                FirstName = passengerDto.FirstName,
+                LastName = passengerDto.LastName,
+                Username = passengerDto.Username,
+                CreatedAt = DateTimeOffset.Now,
+                UpdatedAt = DateTimeOffset.Now,
+            };
+        }
+    }
+}
