636043e211364862a70c2cc9e71930e7fe947b63
Chukwudi Ike-Nwako <ikenwakochukwudi@gmail.com>
2023-11-13 16:43:26 +0100

    added dtos for authentication

---
+++ Dtos/Auth/UserDetailsDto.cs
@@ -0,0 +1,13 @@
+﻿namespace RidePassAPI.Dtos.Auth
+{
+    public class UserDetailsDto
+    {
+        public string? UserId { get; set; }
+        
+        public int? UserType { get; set; }
+        
+        public string? Email { get; set; }
+        
+        public string? Token { get; set; }
+    }
+}
